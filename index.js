c004321bbfc0d3e09a211e403881a880c7bff3b7
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2025-08-21 16:18:13 -0400

    ~çlkjh

---
+++ index.js
@@ -0,0 +1,19 @@
+let DiaDaSemana = 5
+
+if(DiaDaSemana == 1){
+    console.log("Domingo")
+}else if(DiaDaSemana == 2){
+    console.log("Segunda-feira")
+}else if(DiaDaSemana == 3){
+    console.log("Terça-feira")
+}else if(DiaDaSemana == 4){
+    console.log("Quarta-feira")
+}else if(DiaDaSemana == 5){
+    console.log("Quinta-feira")
+}else if(DiaDaSemana == 6){
+    console.log("Sexta-feira")
+}else if(DiaDaSemana == 7){
+    console.log("Sábado")
+}else{
+    console.log("Pode ser qualquer coisa ent")
+}
