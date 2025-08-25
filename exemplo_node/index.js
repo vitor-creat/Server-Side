eddaa93160832b34d147730202e4877346317695
vitor-creat <vitorluciopadilhaoliveira4@gmail.com>
2025-08-24 20:43:37 -0400

    aaa

--- exemplo_node/index.js
+++ exemplo_node/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+app.get("/", (req, res)=> {
+    res.send(`Meu sitezinho`)
+})
+
 app.listen(3000, () => {
     console.log("Servidor Rodando na porta 3000...")
 })
