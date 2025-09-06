c934bb8c6fe6cd68c56a92900b150d928a352bef
vitor-creat <vitorluciopadilhaoliveira4@gmail.com>
2025-09-05 21:23:38 -0400

    não deu

--- exemplo_node/src/app/routes/index.js
+++ exemplo_node/src/app/routes/index.js
@@ -2,10 +2,11 @@ const routes = require('express').Router()
 const get = require('../controller/get')
 const getAlunos = require('../controller/getAlunos')
 const getExercicios = require('../controller/exercicios')
+const getExerciciosEstaticos = require('../controller/exercicios_estaticos')
 routes.get("/", get)
 routes.get('/alunos', getAlunos)
 routes.get('/exercicios', getExercicios)
-routes.get('/exercicios')
+routes.get('/exercicios_estaticos', getExerciciosEstaticos)
 //caminho na url apos o localhost:3000 para saber onde/oq deve ser mostrado na pagina
 //a rota é o complemento da URL após o localhost:3000
 
---
+++ exemplo_node/src/public/scripts.js
@@ -0,0 +1,66 @@
+  //exercicio 1
+let altura = 5
+let base = 10
+area = base*altura
+
+
+//exercicio 2
+let NumeroPar = 2
+let resultadoPar = NumeroPar %2 == 0 ? "O numero é par" : "O numero é impar"
+
+//exercicio 3
+let NumerosAte10String = ""
+for (let i = 1; i <= 10 ; i++) {
+  NumerosAte10String += i + " "
+}
+
+//exercicio 4
+let N = 21;
+let numerosAteNString = " "
+for (let i = 1; i <= N; i++) {
+    numerosAteNString += i + " "
+}
+let verificaMultiplo =  N % 3 == 0 ? "O numero é multiplo de 3" : "O numero não é multiplo de 3" 
+//não sei adaptar essa merda para aparecer, portanto, vai ficar comementado
+// if (N % 3 == 0) {
+//   console.log(`O numero ${N} é multiplo de 3`);
+// } else {
+//   console.log(`O numero ${N} não é multiplo de 3`);
+// }
+
+
+//exercicio 5
+
+
+function eDivisivel(num, divisor) {
+  if (num % divisor == 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+resultadoFunc = eDivisivel(10,3)
+
+
+//exercicio 6
+let numeros = [1,2,3,4,5,10,15,20,22,33,35]
+let Somapares = 0
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    Somapares += numeros[i]//2 4 10 20 22
+  } 
+}
+console.log(Somapares)
+
+//desafio incrivel
+let Str_inv = ""
+function InverterString(Str) {
+  for (let i = 5; i >= 0; i--) {
+    Str_inv += Str[i]
+    
+  }
+  // console.log(Str_inv)
+}
+InverterString("Gundam")
+let resultadoFraseInvertida = Str_inv
