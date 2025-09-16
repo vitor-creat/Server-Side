402c15ed06df65607e6d4dd82781d5c7f7557817
vitor-creat <vitorluciopadilhaoliveira4@gmail.com>
2025-09-16 19:56:22 -0400

    quase terminei

--- exemplo_node/src/app/controller/ex6.js
+++ exemplo_node/src/app/controller/ex6.js
@@ -1,10 +1,11 @@
 module.exports = (req, res) => {
-  let numeros = [1, 2, 3, 4, 5, 10, 15, 20, 22, 33, 35];
+  let numeroString = req.params.numerosArr.split(",");
+  let numeros = numeroString.map(Number)
   let Somapares = 0;
   for (let i = 0; i < numeros.length; i++) {
     if (numeros[i] % 2 === 0) {
       Somapares += numeros[i]; //2 4 10 20 22
     }
   }
-  res.send(`${Somapares}`)
+  res.send(`A soma dos pares é: ${Somapares}`)
 };
--- exemplo_node/src/app/routes/index.js
+++ exemplo_node/src/app/routes/index.js
@@ -5,16 +5,20 @@ const getExercicios = require("../controller/exercicios");
 const getExercicio_estatico = require("../controller/getExercicio");
 const ex1 = require("../controller/ex1");
 const ex2 = require("../controller/ex2");
-
+const ex3 = require("../controller/ex3")
+const ex4 = require("../controller/ex4")
+const ex5 = require("../controller/ex5")
+const ex6 = require("../controller/ex6")
 routes.get("/", get);
 routes.get("/alunos/:nomeAluno/nota1/:notaAluno", getAlunos);
 routes.get("/exercicios", getExercicios);
 routes.get("/exercicios_estaticos", getExercicio_estatico);
 routes.get("/ex1/base/:base/altura/:altura", ex1)
 routes.get("/ex2/numero/:numero", ex2)
-
-
-
+routes.get("/ex3/", ex3)
+routes.get("/ex4/numero/:numero", ex4)
+routes.get("/ex5/numero/:numero/divisor/:divisor", ex5)
+routes.get("/ex6/numeros/:numerosArr", ex6)
 
 
 
