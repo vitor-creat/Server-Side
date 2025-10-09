570e861e726018575fba3928ba641362a291cdab
vitor-creat <vitorluciopadilhaoliveira4@gmail.com>
2025-10-09 13:38:30 -0400

    a

--- exemplo_node/src/config/custom-express.js
+++ exemplo_node/src/config/custom-express.js
@@ -1,7 +1,10 @@
+//chamo o express por meio do require e armazeno em uma express
 const express = require("express");
 
+//chamo a função express e coloco na constante app
 const app = express();
 
+// não sei  
 const path = require("path");
 
 app.use(express.static(path.join(__dirname,  "../public")));
