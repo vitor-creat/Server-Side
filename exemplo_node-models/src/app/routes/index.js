a329a6560cd827741e3cae0600f820a38f1e48ef
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2025-11-05 23:42:01 -0400

    bixo, não sei o que o chat como o chat fez esse ejs

--- exemplo_node-models/src/app/routes/index.js
+++ exemplo_node-models/src/app/routes/index.js
@@ -3,6 +3,8 @@ const get = require("../controller/get");
 const getAlunos = require("../controller/getAlunos");
 const homeController = require("../controller/homeController");
 const aboutController = require("../controller/aboutController");
+const userController = require("../controller/userController")
+const adminController = require("../controller/adminController")
 
 routes.get("/", get);
 routes.get("/alunos/:nomeAluno/nota1/:notaAluno", getAlunos);
@@ -10,6 +12,8 @@ routes.get("/alunos/:nomeAluno/nota1/:notaAluno", getAlunos);
 //
 routes.get('/home_ejs', homeController);
 routes.get('/about_ejs', aboutController);
+routes.get('/user', userController);
+routes.get('/admin', adminController);
 
 
 module.exports = routes;
