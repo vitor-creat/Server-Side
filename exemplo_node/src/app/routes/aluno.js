71abf5c02766f09fb9cc071c7dc364af11e1d7a9
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2025-12-02 13:41:50 -0400

    z

--- exemplo_node/src/app/routes/aluno.js
+++ exemplo_node/src/app/routes/aluno.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
 
-const alunoController = require("../controller/alunoController") 
+const alunoController = require("./") 
 
 routes.get("/listar", alunoController.listar)
 routes.get("/responder/:id", alunoController.mostrarResponder)
